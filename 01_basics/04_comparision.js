ba3169645452c836fe3f7d24108ea30995c39af3
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2023-12-09 13:05:25 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -7,6 +7,7 @@
 
 // console.log("2" > 1);
 // console.log("02" > 1);
+// On inspection the javascript converts the string to a number and displays result
 
 console.log(null > 0);
 console.log(null == 0);
