5f7b2a7ebde32db9ebfda01ef8b85d4fd686e8a8
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2023-12-15 17:25:26 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -17,13 +17,13 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser.email)   // This can access the value of JsUser object's email element but cannot access if it is stored in the string form like this "email"
+// console.log(JsUser["email"])  // This can access the value of JsUser object's email element in both string form "email" and variable form email
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser[mySym])  // This is the only way to access the value of key with type of Symbol in an object
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser)  // Once the freeze method is applied the variable value cannot be changed further it is in freezed state 
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
@@ -34,5 +34,5 @@ JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
+console.log(JsUser.greeting()); // This will print the statement of function greeting but console.log(JsUser.greeting) returns undefined
 console.log(JsUser.greetingTwo());
