8cb04289add5094c663991749fc6fc770028431e
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2023-12-20 18:19:11 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,6 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-
+// For running multiple functions simultaneously they should be seperated by ; indicating the end of IIFE function
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
@@ -8,5 +8,5 @@
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('hitesh') ;
 
