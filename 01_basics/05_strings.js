91e298e4ccc19c89031a0b851a3167e6be8fbbc1
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2024-05-03 12:49:53 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -16,15 +16,15 @@ const gameName = new String('hitesh-hc-com')
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4) // Returns the string between index 0 to 4 
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 4)  // Does not returns anything since index -8 does not exists
 console.log(anotherString);
 
 const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne); // Prints the string as it is with blank spaces
+console.log(newStringOne.trim()); // Trims or removes the blank spaces from string
 // The method trim() removes all whitespaces from string
 
 const url = "https://hitesh.com/hitesh%20choudhary"
