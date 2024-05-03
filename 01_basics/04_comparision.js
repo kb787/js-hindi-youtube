377e3df9f1b8833cdd6950e3d67ad17681830acd
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2024-05-03 12:39:42 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -12,11 +12,14 @@
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+// Returns true for the last case , since null possibility can be greater or equal also but not exactly grater or equal
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
+// Returns false for all case since no comparison is possible for undefined
 
 // === 
 
 console.log("2" === 2);
+// Returns false since the datatype is not same according to === operator 
