155d7b604567ca3cf7977cfcf121193e9f339ecd
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2024-05-09 11:13:52 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,8 +3,8 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(`${this.username} , welcome to website`);  // this.username returns the username parameter of object user
+        console.log(this);   // this can be used to directly reference the entire current user object without calling its name
     }
 
 }
@@ -13,14 +13,14 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this); // Now username is replaced from hitesh -> sam and entire object is returned 
 
 // function chai(){
 //     let username = "hitesh"
 //     console.log(this.username); This can take objects but not variables and returns undefined if applied for variables
 // }
 
-// chai()
+// chai() // Returns blank object for the function chai because the function is not inside object 
 
 // const chai = function () {
 //     let username = "hitesh"
