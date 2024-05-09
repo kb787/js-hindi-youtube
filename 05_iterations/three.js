187fa63aee08997a76128c0db365fecf1dc294cf
Karan Bhanushali <120624674+kb787@users.noreply.github.com>
2024-05-09 12:24:04 +0530

    Update three.js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -5,8 +5,8 @@
 
 const arr = [1, 2, 3, 4, 5]
 
-for (const num of arr) {
-    //console.log(num);
+for (const num of arr) {    // num Is used for traversing individual element for a collection of elements in arr                                                            
+    console.log(num);
 }
 
 const greetings = "Hello world!"
@@ -22,19 +22,17 @@ map.set('USA', "United States of America")
 map.set('Fr', "France")
 map.set('IN', "India")
 
-
-// console.log(map);
+console.log(map); // Displays the output in form of key => value pairs , key = short form , value = full name skipping the repeating objects 
 
 for (const [key, value] of map) {
     // console.log(key, ':-', value);
 }
-
+// Map can be a combination of array => traversing through loops + objects => values stored in form of key, value pairs
 const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
-    
-// }
+ for (const [key, value] of myObject) {  // Throws an error because myObject is an object and not iterable through loops
+    console.log(key, ':-', value);    
+}
